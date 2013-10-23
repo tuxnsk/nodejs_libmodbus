b57916c60bd159a4b1fbc9daa216f5a6d864ebaa
Davydov Denis <mail@tuxnsk.ru>
2013-10-23 14:33:31 +0400

    Improve rollback command in build script

--- make_libmodbus.sh
+++ make_libmodbus.sh
@@ -5,7 +5,7 @@ git clone https://github.com/stephane/libmodbus.git
 cd ./libmodbus/
 
 # reset to patch commit number
-git reset 79a7c1ea82e5bb2e13c8efc77cb3e8a6d7368c58
+git reset --hard 79a7c1ea82e5bb2e13c8efc77cb3e8a6d7368c58
 
 patch -c -p2 < ../mt.patch
 
