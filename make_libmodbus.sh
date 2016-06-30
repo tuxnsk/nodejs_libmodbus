f07ed45da62cab9c87ffce1e737af8c734d89bd1
Alexey Marchenko <a.marchenko@siant.ru>
2016-06-30 21:26:08 +0600

    Delete patch, because with it build generates tons of errors.

--- make_libmodbus.sh
+++ make_libmodbus.sh
@@ -7,7 +7,7 @@ cd ./libmodbus/
 # reset to patch commit number
 git reset --hard 79a7c1ea82e5bb2e13c8efc77cb3e8a6d7368c58
 
-patch -c -p2 < ../mt.patch
+# patch -c -p2 < ../mt.patch
 
 #./autogen.sh && ./configure --enable-static=yes --enable-shared=no && make
 ./autogen.sh && ./configure && make
